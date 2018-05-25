f805d53a32c0dea047d66f51dde62854e6cf96a5
AlvisBlog <alvisblog@alvisblog.xyz>
2018-05-25 12:33:03 +0800

    项目环境

---
+++ env/Scripts/pywin32_testall.py
@@ -0,0 +1,4 @@
+#!c:\users\admin\pycharmprojects\spiderproject_student_003\env\scripts\python.exe
+# EASY-INSTALL-SCRIPT: 'pywin32==221','pywin32_testall.py'
+__requires__ = 'pywin32==221'
+__import__('pkg_resources').run_script('pywin32==221', 'pywin32_testall.py')
